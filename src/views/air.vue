4b864472f32ef62fb658e818f13e07a6b9afb2ba
shirunpei <feiben.dev@gmail.com>
2021-05-11 21:17:31 +0800

    增加空调

---
+++ src/views/air.vue
@@ -0,0 +1,21 @@
+<template>
+  <iframe class="air" src="http://192.168.1.103:3000/" frameborder="0"></iframe>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Air',
+  setup () {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss">
+.air {
+  width: 100%;
+  padding: 0;
+}
+</style>
